4b0fbbeb98302e9e660687594ef06b6be6fe0de1
Bruno Marques <brunomandrade.br@gmail.com>
2020-03-22 18:48:58 -0300

    Adicionando cors ao projeto, fazendo com que nossa api seja acessada publicamnete

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require('cors');
 
 // Iniciando o App
 const app = express();
 
 // Informando minha aplicação a liberação de entradas 'post' de dados em JSON
 app.use(express.json());
+// Dentro do 'cors()' tem configuraçoes de: quais domínios poderão acessar, questões de segurança.. entre outras configurações.
+// A utilização do cors, faz com que a API seja acessada publicamente.
+app.use(cors());
 
 // Iniciando a conexão com o banco de dados
 mongoose.connect('mongodb://localhost:27017/nodeapi',
